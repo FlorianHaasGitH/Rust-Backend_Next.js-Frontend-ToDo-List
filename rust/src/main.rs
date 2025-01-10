586a74a0473f61d8aa711a06dd7d7941faf2d746
Florian-Haas <fhaasbusiness@gmail.com>
2025-01-10 12:14:50 +0100

    Define imports and create `ToDoItem` struct

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,18 @@
+use actix_cors::Cors;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use std::sync::Mutex;
+use chrono::{ Utc,DateTime};
+
+
 fn main {
-    
+
+#[derive(Serialize, Deserialize, Clone)]
+    struct TodDoItem {
+        ui: Uuid,
+        title: String,
+        completed: bool,
+        created_at: DateTime<Utc>
+    }
 }
