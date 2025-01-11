4f6d1462ed4469607a3e9d5e977e6d84391fcc97
Florian-Haas <fhaasbusiness@gmail.com>
2025-01-11 13:45:41 +0100

    Build Corsheader

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,48 +1,43 @@
 use actix_cors::Cors;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
 use std::sync::{Mutex, MutexGuard};
-use chrono::{ Utc, DateTime};
-
-fn main {
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Clone)]
 struct ToDoItem {
     ui: Uuid,
     title: String,
     completed: bool,
-    created_at: DateTime<Utc>
+    created_at: DateTime<Utc>,
 }
 
 #[derive(Deserialize)]
-struct CreateToDoItem{
+struct CreateToDoItem {
     title: String,
     completed: bool,
 }
 
 #[derive(Deserialize)]
-    struct UpdateTodoItem {
-        title: Option<String>,
-        completed: Option<bool>,
-    }
-
-struct AppState {
-    todo_list: Mutex<Vec<ToDoItem>>
+struct UpdateTodoItem {
+    title: Option<String>,
+    completed: Option<bool>,
 }
 
-async fn get_todos(data: web::Data<AppState>) -> impl Responder{
+struct AppState {
+    todo_list: Mutex<Vec<ToDoItem>>,
+}
+
+async fn get_todos(data: web::Data<AppState>) -> impl Responder {
     let todos: MutexGuard<Vec<ToDoItem>> = data.todo_list.lock().unwrap();
     HttpResponse::Ok().json(&*todos)
 }
 
-async fn add_todo(
-    item: web::Json<CreateToDoItem>,
-    data: web::Data<AppState>
-) -> impl Responder {
+async fn add_todo(item: web::Json<CreateToDoItem>, data: web::Data<AppState>) -> impl Responder {
     let mut todos: MutexGuard<Vec<ToDoItem>> = data.todo_list.lock().unwrap();
-    let new_todo = ToDoItem{
-        id: Uuid::new_v4(),
+    let new_todo = ToDoItem {
+        ui: Uuid::new_v4(),
         title: item.title.clone(),
         completed: item.completed,
         created_at: Utc::now(),
@@ -54,30 +49,28 @@ async fn add_todo(
 async fn update_todo(
     path: web::Path<Uuid>,
     item: web::Json<UpdateTodoItem>,
-    data: web::Data<AppState>
+    data: web::Data<AppState>,
 ) -> impl Responder {
-    let mut todos: MutexGuard<Vec<ToDoItem>>;data.todo_list.lock().unwrap();
+    let mut todos: MutexGuard<Vec<ToDoItem>>;
+    data.todo_list.lock().unwrap();
 
-    if let Some(todo) = todos.iter_mut().find(|todo: &&mut ToDoItem|
-        todo.id == *path) {
-            if let Some(title) = &item.title {
-                todo.title = title.clone();
-            }
-            if let Some(completed) = item.completed {
-                todo.completed = completed
-            }
-            HttpResponse::Ok().json(&*todos)
-            } else {
-                HttpResponse::NotFound().body(
-                    "Todo not found"
-                )
-            }
+    if let Some(todo) = todos
+        .iter_mut()
+        .find(|todo: &&mut ToDoItem| todo.id == *path)
+    {
+        if let Some(title) = &item.title {
+            todo.title = title.clone();
+        }
+        if let Some(completed) = item.completed {
+            todo.completed = completed
+        }
+        HttpResponse::Ok().json(&*todos)
+    } else {
+        HttpResponse::NotFound().body("Todo not found")
+    }
 }
 
-async fn delete_todo(
-    path: web::Path<Uuid>,
-    data: web::Data<AppState>
-)   -> impl Responder {
+async fn delete_todo(path: web::Path<Uuid>, data: web::Data<AppState>) -> impl Responder {
     let mut todos = data.todo_list.lock().unwrap();
     if todos.iter().any(|todo: &ToDoItem| todo.id == *path) {
         todos.retain(|todo: &ToDoItem| todo.id != *path);
@@ -85,8 +78,18 @@ async fn delete_todo(
     } else {
         HttpResponse::NotFound().body("Todo not found")
     }
-}
 
+#[active_web::main]
+async fn main() -> std::io::Result<()>{
+    let app_state = web::Data::new(AppState {
+        todo_list: Mutex::new(Vex::new()),
+    })
 
-
+    HttpServer::new(move || {
+        let cors = Cors::default()
+        .allow_any_origin()
+        .allow_any_method()
+        .allow_any_header()
+        .max_age(3600);
+    })
 }
